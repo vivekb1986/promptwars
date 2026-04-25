26cd6aa9a4cbf96502573ebde7f09157b24b7da2
vivekb1986 <vivek.barsaiyan2009@gmail.com>
2026-04-25 14:28:41 +0530

    changes for prompt wars

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { GoogleGenAI } = require("@google/genai");
-const ai = new GoogleGenAI({ apiKey: "AIzaSyBVKO_JSQkINtxo0AoNNk1uFpPBIdcq5dA" });
 
-const ANTHROPIC_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
-// const MODEL             = 'claude-sonnet-4-20250514';
-
+const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
 // ── POST /api/chat ─────────────────────────────────────────────────────────
-// Body: { messages, systemPrompt }
 router.post('/chat', async (req, res) => {
   const { messages, systemPrompt } = req.body;
 
@@ -15,27 +10,36 @@ router.post('/chat', async (req, res) => {
     return res.status(400).json({ error: '`messages` array is required.' });
   }
 
-  if (!process.env.GEMINI_API_KEY) {
-    return res.status(500).json({ error: 'GEMINI_API_KEY is not set on the server.' });
-  }
+  const allMessage = messages[messages.length - 1];
+  const lastMessage = allMessage.content;
+
+
 
   try {
     // Dynamically import node-fetch (ESM package)
     const { default: fetch } = await import('node-fetch');
 
-    const anthropicRes = await fetch(ANTHROPIC_API_URL, {
+    const geminiRes = await fetch(GEMINI_API_URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-goog-api-key': 'AIzaSyBVKO_JSQkINtxo0AoNNk1uFpPBIdcq5dA',
-        // 'anthropic-version' : '2023-06-01',
+        'x-goog-api-key': 'AIzaSyC4kVS0XBs2OXTZYI2oP_raZDphAR4rtI4',
       },
       body: JSON.stringify({
         "contents": [
           {
+            role: "user",
             "parts": [
               {
-                "text": "Explain how AI works in a few words"
+                "text": systemPrompt
+              }
+            ]
+          },
+          {
+            role: "user",
+            "parts": [
+              {
+                "text": lastMessage
               }
             ]
           }
@@ -44,15 +48,13 @@ router.post('/chat', async (req, res) => {
     });
 
 
-
-    if (!anthropicRes.ok) {
-      const errBody = await anthropicRes.text();
+    if (!geminiRes.ok) {
+      const errBody = await geminiRes.text();
       console.error('Gemini API error:', errBody);
-      return res.status(anthropicRes.status).json({ error: 'Gemini API error', detail: errBody });
+      return res.status(geminiRes.status).json({ error: 'Gemini API error', detail: errBody });
     }
-
-    const data = await anthropicRes.json();
-    const text = data.content?.[0]?.text ?? '';
+    const data = await geminiRes.json();
+    const text = data.candidates[0].content.parts[0].text || ""
 
     return res.json({ text });
   } catch (err) {
